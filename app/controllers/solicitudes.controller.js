652215e9ac912e06d116a11a8e889184cc6c614f
Jmolinak <jmolin18@ibero.edu.co>
2022-12-07 17:53:30 -0500

    Creacion servicio PQRS

--- app/controllers/solicitudes.controller.js
+++ app/controllers/solicitudes.controller.js
@@ -1,22 +1,22 @@
-const pqrs= require('../models/pqrs.models');
+const pqrs = require('../models/pqrs.models');
 
-const registrarPQRS=async(req, res)=>{
+const registrarPQRS = async (req, res) => {
     try {
         console.log("En el regidtro de PQRS")
         const solicitud = req.body;
-       let date_ob = new Date();
-       solicitud.fecha=date_ob.getDate();
-    await pqrs.create(solicitud);
+        let date_ob = new Date();
+        solicitud.fecha = date_ob.getDate();
+        await pqrs.create(solicitud);
         return res.status(200).json({
-            ok:true,
+            ok: true,
 
         })
-    }catch (error) {
+    } catch (error) {
         res.status(400).json({
             err: error
         })
     }
 }
-module.exports={
-registrarPQRS
+module.exports = {
+    registrarPQRS
 };
